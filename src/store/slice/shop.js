9299b879b1fe484234228ccf2a2e4faca85b9d5e
Abulkhair <abulkhairmeiramov@gmail.com>
2022-06-16 19:52:15 +0600

    first iteration is finished

--- src/store/slice/shop.js
+++ src/store/slice/shop.js
@@ -4,15 +4,11 @@ export const shopSlice = createSlice({
   name: "shop",
   initialState: {
     category: [],
-    product: [],
     error: null,
   },
   reducers: {
     setCategory(state, action) {
       state.category = action.payload;
-    },
-    setProduct(state, action) {
-      state.product = action.payload;
     },
     setError(state, action) {
       state.error = action.payload;
@@ -20,6 +16,6 @@ export const shopSlice = createSlice({
   },
 });
 
-export const { setCategory, setProduct, setError } = shopSlice.actions;
+export const { setCategory, setError } = shopSlice.actions;
 
 export const shopReducer = shopSlice.reducer;
